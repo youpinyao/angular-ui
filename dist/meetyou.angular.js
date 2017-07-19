c01ff96e9c5b6f0ce3db2c8e03dd9f32714cec35
游品尧 <497400448@qq.com>
2017-07-19 17:12:00 +0800

    placeholder

--- dist/meetyou.angular.js
+++ dist/meetyou.angular.js
@@ -41047,7 +41047,7 @@ module.exports = "<div class=\"nav\">\n  <ul>\n    <li\n      ng-repeat=\"router
 /***/ "IM9K":
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"ma-input ma-date-picker\">\n  <input class=\"ma-input\"\n    date-time\n    ng-readonly=\"true\"\n    ng-model=\"model\"\n    view=\"{{view}}\"\n    date-change=\"changeValue\"\n    min-view=\"{{minView}}\"\n    min-date=\"minDate\"\n    max-date=\"maxDate\"\n    ma-placeholder=\"{{maPlaceholder}}\"\n    format=\"{{format}}\">\n  <ma-icon ma-type=\"calendar\"></ma-icon>\n  <ma-icon ma-type=\"close\" ma-click=\"clear()\" ng-show=\"!!model\" class=\"clear\"></ma-icon>\n  <!--<div date-picker\n    view=\"{{view}}\"\n    ng-model=\"model\"\n    min-view=\"{{minView}}\"\n    format=\"{{format}}\"></div>-->\n</div>\n";
+module.exports = "<div class=\"ma-input ma-date-picker\">\n  <input class=\"ma-input\"\n    date-time\n    ng-readonly=\"true\"\n    ng-model=\"model\"\n    view=\"{{view}}\"\n    date-change=\"changeValue\"\n    min-view=\"{{minView}}\"\n    min-date=\"minDate\"\n    max-date=\"maxDate\"\n    placeholder=\"{{maPlaceholder}}\"\n    format=\"{{format}}\">\n  <ma-icon ma-type=\"calendar\"></ma-icon>\n  <ma-icon ma-type=\"close\" ma-click=\"clear()\" ng-show=\"!!model\" class=\"clear\"></ma-icon>\n  <!--<div date-picker\n    view=\"{{view}}\"\n    ng-model=\"model\"\n    min-view=\"{{minView}}\"\n    format=\"{{format}}\"></div>-->\n</div>\n";
 
 /***/ }),
 
@@ -45311,7 +45311,7 @@ exports[DATA_VIEW] = $DataView;
 /***/ "M4cQ":
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"ma-input ma-date-range-picker\">\n  <input class=\"ma-input\"\n    ng-readonly=\"true\"\n    ma-placeholder=\"{{maPlaceholder}}\"\n    ng-model=\"dateText\">\n  <ma-icon ma-type=\"calendar\"></ma-icon>\n  <ma-icon ma-type=\"close\"\n    ma-click=\"clear()\"\n    ng-show=\"dateText\"\n    class=\"clear\"></ma-icon>\n</div>\n";
+module.exports = "<div class=\"ma-input ma-date-range-picker\">\n  <input class=\"ma-input\"\n    ng-readonly=\"true\"\n    placeholder=\"{{maPlaceholder}}\"\n    ng-model=\"dateText\">\n  <ma-icon ma-type=\"calendar\"></ma-icon>\n  <ma-icon ma-type=\"close\"\n    ma-click=\"clear()\"\n    ng-show=\"dateText\"\n    class=\"clear\"></ma-icon>\n</div>\n";
 
 /***/ }),
 
