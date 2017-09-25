b9ce083e20d95148141e266c0928d56d5ca16c97
游品尧 <497400448@qq.com>
2017-09-25 09:26:02 +0800

    spin文档优化

--- example/pages/components/feedback/spin/index.js
+++ example/pages/components/feedback/spin/index.js
@@ -7,7 +7,12 @@ angular.module('app').controller(controller, mainCtrl);
 mainCtrl.$inject = ['$scope', '$timeout', '$interval', '$message'];
 
 function mainCtrl($scope, $timeout, $interval, $message) {
-
+  $scope.numberTestValue = 120;
+  $scope.codeText = `
+  <ma-spin
+    ma-size="120"
+  ></ma-spin>
+  `;
 }
 
 export default {
