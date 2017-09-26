c8b6e4e40965543c1b8859143b95f8c87f17d0c8
游品尧 <497400448@qq.com>
2017-09-26 14:03:14 +0800

    tabs steps 文档优化

--- example/pages/components/navigation/tabs/index.js
+++ example/pages/components/navigation/tabs/index.js
@@ -9,18 +9,9 @@ mainCtrl.$inject = ['$scope', '$timeout'];
 function mainCtrl($scope, $timeout) {
   $scope.selectTab = 0;
   $scope.selectTab2 = 'default';
-  $scope.defaultTabs =
-    `<ma-tabs ng-model="selectTab">
-  <ma-tab ma-value="0">选项卡1</ma-tab>
-  <ma-tab ma-value="1">选项卡2</ma-tab>
-  <ma-tab ma-value="2">选项卡3</ma-tab>
-</ma-tabs>`;
-  $scope.buttonTabs =
-    `<ma-tabs ng-model="selectTab">
-  <ma-tab-button ma-size="{{selectTab}}" ma-value="large">选项卡 大</ma-tab-button>
-  <ma-tab-button ma-size="{{selectTab}}" ma-value="default">选项卡 中</ma-tab-button>
-  <ma-tab-button ma-size="{{selectTab}}" ma-value="small">选项卡 小</ma-tab-button>
-</ma-tabs>`;
+
+  $scope.codeText = require('./code.html');
+
   $timeout(() => {
     $scope.selectTab = 1;
   }, 1000);
