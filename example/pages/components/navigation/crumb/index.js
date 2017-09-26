8b0898e3ed01334cba485d4c3f5071eb2654ff30
游品尧 <497400448@qq.com>
2017-09-26 10:05:36 +0800

    crumb 文档优化

--- example/pages/components/navigation/crumb/index.js
+++ example/pages/components/navigation/crumb/index.js
@@ -7,11 +7,7 @@ angular.module('app').controller(controller, mainCtrl);
 mainCtrl.$inject = ['$scope'];
 
 function mainCtrl($scope) {
-  $scope.normalCrumb = `<ma-crumb>
-  <ma-crumb-item ma-href="#!/components">Components 组件</ma-crumb-item>
-  <ma-crumb-item ma-href="#!/components/global">Global 全局</ma-crumb-item>
-  <ma-crumb-item>Font 文字</ma-crumb-item>
-</ma-crumb>`;
+  $scope.codeText = require('./code.html');
 
   $scope.autoCrumb = '<ma-crumb-auto></ma-crumb-auto>';
 }
