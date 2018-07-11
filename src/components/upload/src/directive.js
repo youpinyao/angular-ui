7768767d66b72d2a00dac052ac1f315a857666fb
游品尧 <497400448@qq.com>
2018-07-11 14:41:17 +0800

    fix: upload组件去除中文文件名

--- src/components/upload/src/directive.js
+++ src/components/upload/src/directive.js
@@ -200,8 +200,6 @@ function _maUpload($compile, FileUploader, $message, $utils, template, defaultCo
         // eslint-disable-next-line
         let key = '${filename}';
 
-        key = `${$utils.uuid()}_${key}`;
-
         if (ossConfig.dir) {
           key = `${ossConfig.dir}/${key}`;
         }
@@ -308,6 +306,12 @@ function _maUpload($compile, FileUploader, $message, $utils, template, defaultCo
 
     function onBeforeUploadItem(fileItem) {
       // console.log('onBeforeUploadItem---', '[', fileItem._file.name, ']');
+      // 中文编码
+      let name = fileItem.file.name || '';
+      name = name.split('.');
+
+      fileItem.file.name = `${$utils.uuid()}.${name[name.length - 1]}`;
+
       scope.ngModel.push({
         file: fileItem._file,
         name: fileItem._file.name,
